1064644550a7871c98cbfc11d85cc285b28d8b5f
saikiran <sai.saikiran725@gmail.com>
2022-03-07 01:07:56 +0530

    fghj

--- Start With Terraform Basics/createInstance.tf
+++ Start With Terraform Basics/createInstance.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-  access_key = "AKIAXC3GVBM2KKKM7NKZ"
-  secret_key = "SECRET_KEY_HERE"
-  region     = "us-east-2"
-}
-
 resource "aws_instance" "FirstInstance" {
   ami          = 	"ami-05803413c51f242b7"
   instance_type = "t2.micro"
