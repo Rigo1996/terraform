9e7a7fccdfad7d6a50455a06c9f09c131aa45777
saikiran <sai.saikiran725@gmail.com>
2022-03-07 01:30:53 +0530

    qwsdfg

--- Start With Terraform Basics/createInstance.tf
+++ Start With Terraform Basics/createInstance.tf
@@ -1,5 +1,10 @@
 resource "aws_instance" "FirstInstance" {
+  count        =   3
   ami          = 	"ami-05803413c51f242b7"
   instance_type = "t2.micro"
+
+  tags = {
+    Name= "threedemoinstances"
+  }
   
 }
