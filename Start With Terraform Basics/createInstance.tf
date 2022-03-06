b7c12149adeb195d2f25cbd95835356305d08275
saikiran <sai.saikiran725@gmail.com>
2022-03-07 00:15:54 +0530

    qwertyu

--- Start With Terraform Basics/createInstance.tf
+++ Start With Terraform Basics/createInstance.tf
@@ -1,10 +1,11 @@
+provider "aws" {
+  access_key = "AKIAXC3GVBM2KKKM7NKZ"
+  secret_key = "SECRET_KEY_HERE"
+  region     = "us-east-2"
+}
 
-resource "aws_instance" "MyFirstInstnace" {
-  count         = 3
-  ami           = "ami-0bff25b43a4479334"
-  instance_type = "t4g.micro"
-
-  tags = {
-    Name = "demoinstnce-${count.index}"
-  }
+resource "aws_instance" "FirstInstance" {
+  ami          = 	"ami-05803413c51f242b7"
+  instance_type = "t2.micro"
+  
 }
