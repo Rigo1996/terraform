8c28d58010e70c0c7a0e17359c68fc60963bd3f3
saikiran <sai.saikiran725@gmail.com>
2022-03-07 23:34:14 +0530

    asdf

--- Terraform Concepts Building Blocks/Demo DataSource/createInstance.tf
+++ Terraform Concepts Building Blocks/Demo DataSource/createInstance.tf
@@ -1,8 +1,9 @@
-
+data "aws_availability_zones" "available" {}
 
 resource "aws_instance" "FirstInstance" {
   ami           = lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
+  availability_zone = data.aws_availability_zones.available.names[1]
   
 
   tags = {
