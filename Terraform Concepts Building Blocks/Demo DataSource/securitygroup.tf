7ed5805938753b701cfc186a99eb6d24cb0c239e
saikiran <sai.saikiran725@gmail.com>
2022-03-09 11:22:51 +0530

    qwertytyuj

--- Terraform Concepts Building Blocks/Demo DataSource/securitygroup.tf
+++ Terraform Concepts Building Blocks/Demo DataSource/securitygroup.tf
@@ -1,6 +1,7 @@
 data "aws_ip_ranges" "us_east_ip_range" {
     regions = ["us-east-1","us-east-2"]
     services = ["ec2"]
+}
 
 resource "aws_security_group" "sg-custom_us_east" {
     name = "sg-custom_us_east"
@@ -16,5 +17,4 @@ resource "aws_security_group" "sg-custom_us_east" {
         CreateDate = data.aws_ip_ranges.us_east_ip_range.create_date
         SyncToken = data.aws_ip_ranges.us_east_ip_range.sync_token
     }
-}
 }
