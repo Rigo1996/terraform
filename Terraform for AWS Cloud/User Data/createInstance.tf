482244f05c693119de4b454c0e203c8c89cf0a07
saikiran <sai.saikiran725@gmail.com>
2022-03-11 19:41:43 +0530

    ertyu

---
+++ Terraform for AWS Cloud/User Data/createInstance.tf
@@ -0,0 +1,27 @@
+
+resource "aws_key_pair" "levelup_key" {
+  key_name = "levelup_key"
+  public_key = file(var.PATH_TO_PUBLIC_KEY)
+  
+}
+
+resource "aws_instance" "FirstInstance" {
+  ami           = lookup(var.AMIS, var.AWS_REGION)
+  instance_type = "t2.micro"
+  availability_zone = "us-east-2a"
+  key_name      =  aws_key_pair.levelup_key.key_name
+
+
+  user_data = file("installapche.sh")
+
+
+
+
+
+  tags = {
+    Name = "custom_instance"
+  }
+
+}
+
+  
