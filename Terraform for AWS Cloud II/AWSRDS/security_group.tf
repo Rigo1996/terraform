4d10ca26840ea2a3e21733adf9dfa9f987a1afe4
saikiran <sai.saikiran725@gmail.com>
2022-03-14 10:14:12 +0530

    wert

--- Terraform for AWS Cloud II/AWSRDS/security_group.tf
+++ Terraform for AWS Cloud II/AWSRDS/security_group.tf
@@ -4,16 +4,16 @@ resource "aws_security_group" "allow-levelup-ssh" {
   vpc_id      = aws_vpc.levelupvpc.id
 
   ingress {
-    from_port        = 0
-    to_port          = 0
+    from_port        = 22
+    to_port          = 22
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     
   }
 
   egress {
-    from_port        = 22
-    to_port          = 22
+    from_port        = 0
+    to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
    
