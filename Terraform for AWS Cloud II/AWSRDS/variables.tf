cba74e49739cf43e4c6a9d59f0bee0d439dd622b
saikiran <sai.saikiran725@gmail.com>
2022-03-14 09:46:36 +0530

    rtyui

--- Terraform for AWS Cloud II/AWSRDS/variables.tf
+++ Terraform for AWS Cloud II/AWSRDS/variables.tf
@@ -1,6 +1,6 @@
 variable "AWS_ACCESS_KEY" {
     type = string
-    default = "AKIASMSIZOF42P2VUDSZ"
+    default = "AKIAXC3GVBM2FLULOWQ4"
 }
 
 variable "AWS_SECRET_KEY" {}
@@ -9,15 +9,18 @@ variable "AWS_REGION" {
 default = "us-east-2"
 }
 
+
+
 variable "AMIS" {
     type = map
     default = {
-        us-east-1 = "ami-0f40c8f97004632f9"
-        us-east-2 = "ami-05692172625678b4e"
         us-west-2 = "ami-0352d5a37fb4f603f"
         us-west-1 = "ami-0f40c8f97004632f9"
+        us-east-1 = "ami-0b0ea68c435eb488d"
+        us-east-2 = "ami-05803413c51f242b7"
     }
 }
+
 
 variable "PATH_TO_PRIVATE_KEY" {
   default = "levelup_key"
@@ -30,3 +33,4 @@ variable "PATH_TO_PUBLIC_KEY" {
 variable "INSTANCE_USERNAME" {
   default = "ubuntu"
 }
+
